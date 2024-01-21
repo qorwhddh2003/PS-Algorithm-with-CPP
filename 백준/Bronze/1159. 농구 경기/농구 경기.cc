db9d7840c5c31332bb7a107977dc8a2bf754a9e7
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-01-21 23:49:46 +0900

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.cc
+++ 백준/Bronze/1159. 농구 경기/농구 경기.cc
@@ -2,22 +2,17 @@
 using namespace std;
 
 int n, cnt[26];
-string str;
-bool flag = 0;
+string str, ret;
 int main(){
     cin >> n;
     for(int i = 0; i < n; i++){
         cin >> str;
-        cnt[(char)str[0] - 'a']++;
+        cnt[str[0] - 'a']++;
     }
-    for(int i = 0; i < 26; i++){
-        if(cnt[i] >= 5){
-            flag = 1;
-            cout << (char)(i + 'a');
-        }
-    }
+    for(int i = 0; i < 26; i++) if(cnt[i] >= 5) ret += (i + 'a');
     
-    if(flag == 0) cout << "PREDAJA";
+    if(ret.size()) cout << ret;
+    else cout << "PREDAJA";
     
     return 0;
 }
