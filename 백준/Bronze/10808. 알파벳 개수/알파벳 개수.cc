1c770b93bd606df5ce0fba7a8110d8d14b296435
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-01-22 19:21:09 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -4,13 +4,10 @@ using namespace std;
 int cnt[26];
 string str;
 int main(){
-    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-    
     cin >> str;
     for(char c : str){
         cnt[c - 'a']++;
     }
-    for(int i : cnt) cout << i << " ";
     
-    return 0;
+    for(int i : cnt) cout << i << " ";
 }
