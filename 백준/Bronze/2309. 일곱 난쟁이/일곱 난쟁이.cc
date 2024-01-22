25498c06121f2c9a6b113a3b9a7a9b1b4ebdd2f2
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-01-22 19:18:45 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,33 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a[9];
-void printV(vector<int> v){
-    for(int i : v) cout << i << '\n';
-}
-
-void combi(int start, vector<int> &b){
-    if(b.size() == 7){
-        int sum = accumulate(b.begin(), b.end(), 0);
-        if(sum == 100){
-            printV(b);
-            exit(0);
-        }
-    }
-    
-    for(int i = start+1; i < 9; i++){
-        b.push_back(a[i]);
-        combi(i, b);
-        b.pop_back();
-    }
-    
-    return;
-}
-
+int val;
+vector<int> dwarf;
 int main(){
-    for(int i = 0; i < 9; i++) cin >> a[i];
-    sort(a, a+9);
-    vector<int> v;
-    combi(-1, v);
+    for(int i = 0; i < 9; i++){
+        cin >> val;
+        dwarf.push_back(val);
+    }
+    sort(dwarf.begin(), dwarf.end());
+    
+    do{
+        int sum = accumulate(dwarf.begin(), dwarf.begin() + 7, 0);
+        if(sum == 100) break;
+    }while(next_permutation(dwarf.begin(), dwarf.end()));
+    
+    for(int i = 0; i < 7; i++) cout << dwarf[i] << '\n';
+    
     return 0;
 }
