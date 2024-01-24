049e506624e97219e0616b16232ecc4fa3b03a99
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-01-24 22:21:49 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -1,33 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int odd_num;
-string s, ret;
+int cnt[26], flag;
+string name, ret;
+char mid;
 int main(){
-    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-    cin >> s;
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
     
-    map<char, int> mp;
-    for(char ch : s){
-        mp[ch]++;
-    }
-    
-    for(auto it = mp.rbegin(); it != mp.rend(); it++){
-        char key = (*it).first;
-        int value = (*it).second;
-        
-        if(value % 2 != 0) {
-            ret = ret.substr(0, ret.size() / 2) + key + ret.substr(ret.size() / 2);
-            odd_num++;
-            value--;
+    cin >> name;
+    for(char c : name) cnt[c - 'A']++;
+    for(int i = 'Z'; i >= 'A'; i--){
+        if(cnt[i - 'A'] & 1) {
+            mid = char(i);
+            cnt[i - 'A']--;
+            flag++;
+            
         }
-        
-        for(int i = 0; i < value; i++){
-            if(i % 2 == 0) ret = key + ret;
-            else ret += key;
+        if(flag == 2) break;
+        for(int j = 0; j < cnt[i - 'A']; j += 2){
+            ret = char(i) + ret;
+            ret += char(i);
         }
     }
+    if(flag == 2) cout << "I'm Sorry Hansoo";
+    else{
+        if(mid) ret.insert(ret.begin() + ret.size() / 2, mid);
+        cout << ret;
+    }
     
-    if(odd_num > 1) cout << "I'm Sorry Hansoo";
-    else cout << ret << '\n';
+    return 0;
 }
