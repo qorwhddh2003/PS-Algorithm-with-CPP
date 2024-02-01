c6808a468e41cc4abeb48a0f49175b4d16f150da
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-02-01 23:09:56 +0900

    [Silver V] Title: 사과 담기 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.cc
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.cc
@@ -1,25 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, j, ret;
-int apple_loc;
-
+int n, m, j, ret, temp;
 int main(){
-    cin >> n >> m >> j;
-    int cur = 1;
-    while(j--){
-        cin >> apple_loc;
-        
-        if(cur <= apple_loc && (cur + (m-1)) >= apple_loc) continue;
-        while(true){
-            if(cur < apple_loc) cur++;
-            else cur--;
-            ret++;
-            
-            if(cur <= apple_loc && cur + (m-1) >= apple_loc) break;
+    cin >> n >> m >> j;    
+    int l = 1, r, temp;
+    for(int i = 0; i < j; i++){
+        r = l + m - 1;
+        cin >> temp;
+        if(temp >= l && temp <= r) continue;
+        if(l > temp){
+            ret += l - temp;
+            l = temp;
+        }else if(l < temp){
+            ret += temp - r;
+            l += temp - r;
         }
     }
     
     cout << ret;
-    return 0;
+    return 0;    
 }
