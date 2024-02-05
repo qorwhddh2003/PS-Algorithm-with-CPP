abb02f576c3598d3aa146a7f2b131dcbd1017db6
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-02-05 22:11:51 +0900

    [Silver III] Title: 교수가 된 현우, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -1,27 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int t, n;
+int T, N;
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cin >> t;
-    while(t--){
-        cin >> n;
-        int ret2 = 0, ret5 = 0;
-        
-        for(int i = 2; i <= n; i *= 2){
-            ret2 += n / i;
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+    cin >> T;
+    while(T--){
+        cin >> N;
+        int five_cnt = 0;
+        for(int i = 5; i <= N; i *= 5){
+            five_cnt += N / i;
         }
-        
-        for(int i = 5; i <= n; i *= 5){
-            ret5 += n / i;
-        }
-        
-        cout << min(ret2, ret5) << "\n";
+        cout << five_cnt << "\n";
     }
-    
-    
     return 0;
 }
