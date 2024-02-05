c86d20fa93879b44ecc8fcee7de91b4bc6c08572
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-02-05 21:59:48 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
@@ -1,52 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string s;
-int main(){
-    while(cin >> s){
+bool IsVowel(int idx){
+    return (idx == 'a' || idx == 'e' || idx == 'i' || idx == 'o' || idx == 'u');
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+    string s;
+    while(true){
+        cin >> s;
         if(s == "end") break;
-        int vw_cnt = 0, con_cnt = 0, idx = 0;
-        bool vw_flag = 0, ret_flag = 1;
-        
-        do{
-            char temp = s[idx];
-            switch(temp){
-                case 'a':
-                case 'e':
-                case 'i':
-                case 'o':
-                case 'u': {
-                    vw_cnt++;
-                    con_cnt = 0;
-                    vw_flag = 1;
-                    break;
-                }
-                default: {
-                    vw_cnt = 0;
-                    con_cnt++;
-                }
-            }
-            
-            if(vw_cnt == 3 || con_cnt == 3){
-                ret_flag = 0;
-                break;
-            }else if(idx + 1 <= s.size()-1){
-                if(temp == s[idx + 1]){
-                    if(temp == 'e' || temp == 'o'){
-                        idx++;
-                        continue;
-                    }
-                    ret_flag = 0;
-                    break;
-                }
-            }
-            
-            idx++;
-        }while(idx < s.size());
-        if(vw_flag == 0) ret_flag = 0;
-        if(ret_flag) cout << "<" << s << "> is acceptable.\n";
-        else cout << "<" << s << "> is not acceptable.\n";
+        int vw_cnt = 0, con_cnt = 0, prev = -1;
+        bool flag = 0, vw_flag = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(IsVowel(s[i])) vw_flag = 1, vw_cnt++, con_cnt = 0;
+            else vw_cnt = 0, con_cnt++;
+            if(vw_cnt == 3 || con_cnt == 3) flag = 1;
+            if(i >= 1 && s[i] == s[prev] && (s[i] != 'e' && s[i] != 'o')) flag = 1;
+            prev = i;
+        }
+        if(vw_flag == 0) flag = 1;
+        if(flag) cout << "<"  << s << "> is not acceptable.\n";
+        else cout << "<" << s << "> is acceptable.\n";
     }
-    
     return 0;
 }
