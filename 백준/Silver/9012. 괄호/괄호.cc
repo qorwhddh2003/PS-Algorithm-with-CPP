04746b3ca23d36608f5d801e69bde52bf2d2fae6
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-02-05 23:48:43 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -3,26 +3,26 @@ using namespace std;
 
 int T;
 string s;
+
+bool check(string s){
+    stack<char> stk;
+    for(char c : s){
+        if(c == '(') stk.push('(');
+        else{
+            if(stk.size()) stk.pop();
+            else return false;
+        }
+    }
+    return stk.empty();
+}
+
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
     cin >> T;
     while(T--){
         cin >> s;
-        bool flag = 0;
-        stack<char> stk;
-        for(char c : s){
-            if(c == '('){
-                stk.push('(');
-            }else{
-                if(stk.size()) stk.pop();
-                else{
-                    flag = 1;
-                    break;
-                }
-            }
-        }
-        if(flag == 1 || stk.size()) cout << "NO\n";
-        else cout << "YES\n";
+        if(check(s)) cout << "YES\n";
+        else cout << "NO\n";
     }
     return 0;
 }
