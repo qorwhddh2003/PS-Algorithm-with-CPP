a243b35e888ed0cbc8f4b0130f0fc512b61fe0bc
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-02-11 14:56:19 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
@@ -1,29 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool IsVowel(int idx){
-    return (idx == 'a' || idx == 'e' || idx == 'i' || idx == 'o' || idx == 'u');
+bool isVowel(int idx) {
+	return (idx == 'a' || idx == 'e' || idx == 'i' || idx == 'o' || idx == 'u');
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-    string s;
-    while(true){
-        cin >> s;
-        if(s == "end") break;
-        int vw_cnt = 0, con_cnt = 0, prev = -1;
-        bool flag = 0, vw_flag = 0;
-        for(int i = 0; i < s.size(); i++){
-            if(IsVowel(s[i])) vw_flag = 1, vw_cnt++, con_cnt = 0;
-            else vw_cnt = 0, con_cnt++;
-            if(vw_cnt == 3 || con_cnt == 3) flag = 1;
-            if(i >= 1 && s[i] == s[prev] && (s[i] != 'e' && s[i] != 'o')) flag = 1;
-            prev = i;
-        }
-        if(vw_flag == 0) flag = 1;
-        if(flag) cout << "<"  << s << "> is not acceptable.\n";
-        else cout << "<" << s << "> is acceptable.\n";
-    }
-    return 0;
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string s;
+	while (true)
+	{
+		cin >> s;
+		if (s == "end") break;
+		int vw_cnt = 0, con_cnt = 0, vw_flag = 0, flag = 0, prev = -1;
+		for (int i = 0; i < s.size(); i++) {
+			if (isVowel(s[i])) vw_cnt++, con_cnt = 0, vw_flag = 1;
+			else con_cnt++, vw_cnt = 0;
+			if (vw_cnt == 3 || con_cnt == 3) flag = 1;
+			if (i >= 1 && s[i] == s[prev] && (s[i] != 'e' && s[i] != 'o')) flag = 1;
+			prev = i;
+		}
+		if (vw_flag == 0) flag = 1;
+		if (flag) cout << "<" << s << "> is not acceptable.\n";
+		else cout << "<" << s << "> is acceptable.\n";
+	}
+	return 0;
 }
