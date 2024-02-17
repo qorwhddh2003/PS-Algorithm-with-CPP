7981b1f816804937e0c5264d04711e95f4a4f8ca
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-02-17 22:12:09 +0900

    [Gold IV] Title: 주난의 난(難), Time: 4 ms, Memory: 2476 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.cc
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.cc
@@ -1,76 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define y1 aaaaa
+#define y1 aaaa
 
-int n, m, x1, y1, x2, y2, ret, yy, xx;
-bool flag = 0;
-char a[300][300];
+int n, m, x1, y1, x2, y2;
 int visited[300][300];
+char a[301][301];
 string s;
 int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, 1, 0, -1};
 
-void bfs(int y, int x){
-    queue<pair<int, int>> q;
-    queue<pair<int, int>> qq;
-    q.push({y, x});
-    visited[y][x] = 1;
-    
-    while(q.size()){
-        tie(y, x) = q.front();
-        q.pop();
-        
-        if(y == y2 - 1 && x == x2 - 1){
-            ret = visited[y][x] - 1;
-            break;
-        }
-        
-        for(int i = 0; i < 4; i++){
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if(ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx]) continue;
-            if(a[ny][nx] == '0'){
-                visited[ny][nx] = visited[y][x];
-                qq.push({ny, nx});
-            }
-            else{
-                visited[ny][nx] = visited[y][x] + 1;
-                q.push({ny, nx});
-            }
-        }
-        
-        while(qq.size()){
-            tie(yy, xx) = qq.front();
-            qq.pop();
-            
-            if(yy == y2 - 1 && xx == x2 - 1){
-                flag = 1;
-                ret = visited[yy][xx];
-                break;
-            }
-            
-            for(int i = 0; i < 4; i++){
-                int nny = yy + dy[i];
-                int nnx = xx + dx[i];
-                if(nny < 0 || nnx < 0 || nny >= n || nnx >= m || visited[nny][nnx]) continue;
-                if(a[nny][nnx] == '0' || a[nny][nnx] == '#'){
-                    visited[nny][nnx] = visited[y][x];
-                    qq.push({nny, nnx});
-                }else{
-                    visited[nny][nnx] = visited[y][x] + 1;
-                    q.push({nny, nnx});
-                }
-            }
-        }
-        if(flag) break;
-    }
-}
-
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    cout.tie(NULL);
+int main()
+{
     cin >> n >> m;
     cin >> y1 >> x1 >> y2 >> x2;
+    y1--, x1--, y2--, x2--;
     
     for(int i = 0; i < n; i++){
         cin >> s;
@@ -78,7 +20,31 @@ int main(){
             a[i][j] = s[j];
         }
     }
-    bfs(y1 - 1, x1 - 1);
-    cout << ret;
+    
+    queue<int> q;
+    q.push(y1 * 1000 + x1);
+    visited[y1][x1] = 1;
+    int cnt = 0;
+    while(a[y2][x2] != '0'){
+        cnt++;
+        queue<int> temp;
+        while(q.size()){
+            int y = q.front() / 1000;
+            int x = q.front() % 1000;
+            q.pop();
+            for(int i = 0; i < 4; i++){
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+                if(ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx]) continue;
+                visited[ny][nx] = cnt;
+                if(a[ny][nx] != '0'){
+                    a[ny][nx] = '0';
+                    temp.push(1000 * ny + nx);
+                }else q.push(1000 * ny + nx);
+            }
+        }
+        q = temp;
+    }
+    cout << visited[y2][x2];
     return 0;
 }
