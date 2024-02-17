6528dbaf90c4c7462a9a813f227bde3fc49fb05f
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-02-17 16:03:40 +0900

    [Gold IV] Title: 인구 이동, Time: 80 ms, Memory: 2088 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.cc
+++ 백준/Gold/16234. 인구 이동/인구 이동.cc
@@ -1,55 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, L, R, day, sum, cnt;
+int n, l, r, ret;
+bool flag = 0;
 int a[50][50], visited[50][50];
 int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, 1, 0, -1};
+vector<pair<int, int>> v;
 
 void dfs(int y, int x, vector<pair<int, int>> &v){
     visited[y][x] = 1;
     for(int i = 0; i < 4; i++){
         int ny = y + dy[i];
         int nx = x + dx[i];
-        
-        if(ny < 0 || nx < 0 || ny >= N || nx >= N || visited[ny][nx]) continue;
-        if(abs(a[ny][nx] - a[y][x]) >= L && abs(a[ny][nx] - a[y][x]) <= R){
-            sum += a[ny][nx];
-            v.push_back({ny, nx});
+        if(ny < 0 || nx < 0 || ny >= n || nx >= n || visited[ny][nx]) continue;
+        if(abs(a[ny][nx] - a[y][x]) >= l && abs(a[ny][nx] - a[y][x]) <= r){
             dfs(ny, nx, v);
+            v.push_back({ny, nx});
         }
     }
 }
 
 int main(){
-    ios_base::sync_with_stdio(0); cin.tie(NULL);
-    cin >> N >> L >> R;
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
+    cin >> n >> l >> r;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
             cin >> a[i][j];
         }
     }
     
     while(true){
-        bool flag = 0;
+        flag = 0;
         fill(&visited[0][0], &visited[0][0] + 50 * 50, 0);
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
                 if(visited[i][j]) continue;
-                vector<pair<int, int>> v;
+                v.clear();
                 v.push_back({i, j});
-                sum = a[i][j];
                 dfs(i, j, v);
-                if(v.size() == 1) continue;
-                for(pair<int, int> it : v){
-                    a[it.first][it.second] = sum / v.size();
-                    flag = 1;
+                int sum = 0;
+                int vSize = v.size();
+                if(vSize == 1) continue;
+                for(auto it : v){
+                    sum += a[it.first][it.second];
                 }
+                sum /= vSize;
+                for(auto it : v){
+                    a[it.first][it.second] = sum;
+                }
+                flag = 1;
             }
         }
-        if(!flag) break;
-        day++;
+        if(flag == 0) break;
+        ret++;
     }
     
-    cout << day;
+    cout << ret;
     return 0;
 }
