2b726a80b53e5bda4d7bf93ce6ed15f366daaea9
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-02-18 22:02:36 +0900

    [Silver I] Title: 효율적인 해킹, Time: 3548 ms, Memory: 3540 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, M, A, B, ret;
-int visited[10001];
-vector<int> adj[10001], v;
+int n, m, a, b, mx;
+int visited[10001], dp[10001];
+vector<int> adj[10001];
 
-int dfs(int here){
+int go(int here){
     int ret = 1;
-    visited[here] = 1;
+    visited[here]  = 1;
     for(int there : adj[here]){
         if(visited[there]) continue;
-        ret += dfs(there);
+        ret += go(there);
     }
     return ret;
 }
@@ -18,22 +18,21 @@ int dfs(int here){
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
-    cin >> N >> M;
-    for(int i = 0; i < M; i++){
-        cin >> A >> B;
-        adj[B].push_back(A);
+    cout.tie(NULL);
+    cin >> n >> m;
+    
+    for(int i = 0; i < m; i++){
+        cin >> a >> b;
+        adj[b].push_back(a);
     }
     
-    for(int i = 1; i <= N; i++){
-        fill(&visited[0], &visited[10001], 0);
-        int cnt = dfs(i);
-        if(cnt > ret){
-            v.clear();
-            v.push_back(i);
-            ret = cnt;
-        }else if(cnt == ret){
-            v.push_back(i);
-        }
+    for(int i = 1; i <= n; i++){
+        fill(&visited[0], &visited[0] + 10001, 0);
+        dp[i] = go(i);
+        mx = max(mx, dp[i]);
     }
-    for(int i : v) cout << i << " ";
+    
+    for(int i = 1; i <= n; i++){
+        if(mx == dp[i]) cout << i << " ";
+    }
 }
