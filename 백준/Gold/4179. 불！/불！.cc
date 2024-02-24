b0f67e98847cb5754018ef8d4fc588969981617b
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-02-24 11:14:19 +0900

    [Gold IV] Title: 불!, Time: 44 ms, Memory: 10944 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,79 +1,71 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int r, c, ret;
-int ji_visited[1000][1000], f_visited[1000][1000];
-char a[1000][1000];
+int r, c, x, y, sx, sy, ret=-1;
 string s;
-pair<int, int> ji_hoon;
-vector<pair<int, int>> fire;
+int fire_visited[1000][1000], visited[1000][1000];
+char a[1000][1000];
 int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, 1, 0, -1};
-
-void fire_bfs(vector<pair<int, int>> v){
-    fill(&f_visited[0][0], &f_visited[0][0] + 1000 * 1000, 1e6);
-    queue<pair<int, int>> q;
-    for(auto i : v){
-        q.push({i.first, i.second});
-        f_visited[i.first][i.second] = 1;
-    }
-    
-    int y = 0, x = 0;
-    while(q.size()){
-        tie(y, x) = q.front(); q.pop();
-        for(int i = 0; i < 4; i++){
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if(ny < 0 || nx < 0 || ny >= r || nx >= c) continue;
-            if(f_visited[ny][nx] != 1e6 || a[ny][nx] == '#') continue;
-            q.push({ny, nx});
-            f_visited[ny][nx] = f_visited[y][x] + 1;
-        }
-    }
-}
-
-int bfs(int y, int x){
-    queue<pair<int, int>> q;
-    q.push({y, x});
-    ji_visited[y][x] = 1;
-    
-    while(q.size()){
-        tie(y, x) = q.front(); q.pop();
-        
-        if(y == r-1 || x == c-1 || y == 0 || x == 0){
-            return ji_visited[y][x];
-        }
-        
-        for(int i = 0; i < 4; i++){
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            
-            if(ji_visited[ny][nx] || a[ny][nx] == '#') continue;
-            if(ji_visited[y][x] + 1 < f_visited[ny][nx]){
-                ji_visited[ny][nx] = ji_visited[y][x] + 1;
-                q.push({ny, nx});
-            }
-        }
-    }
-    return -1;
-}
+queue<pair<int, int>> fq, q;
 
 int main(){
-    ios_base::sync_with_stdio(0);
+    ios_base::sync_with_stdio(0); 
     cin.tie(NULL);
-    
     cin >> r >> c;
     for(int i = 0; i < r; i++){
         cin >> s;
         for(int j = 0; j < c; j++){
             a[i][j] = s[j];
-            if(a[i][j] == 'J') ji_hoon = {i, j};
-            else if(a[i][j] == 'F') fire.push_back({i, j});
         }
     }
     
-    fire_bfs(fire);
-    ret = bfs(ji_hoon.first, ji_hoon.second);
-    if(ret == -1) cout << "IMPOSSIBLE";
+    fill(&fire_visited[0][0], &fire_visited[0][0] + 1000 * 1000, 1e6);
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            if(a[i][j] == 'F') {
+                fq.push({i, j});
+                fire_visited[i][j] = 1;
+            }
+            else if(a[i][j] == 'J') sy = i, sx = j;
+        }
+    }
+    
+    while(fq.size()){
+        tie(y, x) = fq.front();
+        fq.pop();
+        
+        for(int i = 0; i < 4; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            
+            if(ny < 0 || nx < 0 || ny >= r || nx >= c) continue;
+            if(fire_visited[ny][nx] != 1e6 || a[ny][nx] == '#') continue;
+            fire_visited[ny][nx] = fire_visited[y][x] + 1;
+            fq.push({ny, nx});
+        }
+    }
+    
+    q = queue<pair<int, int>>();
+    q.push({sy, sx});
+    visited[sy][sx] = 1;
+    while(q.size()){
+        tie(y, x) = q.front();
+        q.pop();
+        
+        if(y == 0 || x == 0 || y == r - 1 || x == c - 1){
+            ret = visited[y][x];
+            break;
+        }
+        for(int i = 0; i < 4; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(visited[ny][nx] || a[ny][nx] == '#') continue;
+            if(fire_visited[ny][nx] <= visited[y][x] + 1) continue;
+            visited[ny][nx] = visited[y][x] + 1;
+            q.push({ny, nx});
+        }
+    }
+    if(ret==-1) cout << "IMPOSSIBLE";
     else cout << ret;
     return 0;
 }
