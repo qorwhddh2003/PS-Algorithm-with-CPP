1e261ccb46065fb7998c3ed3d368b4554401e7f8
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-02-28 23:16:55 +0900

    [Gold III] Title: 종이 조각, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14391. 종이 조각/종이 조각.cc
+++ 백준/Gold/14391. 종이 조각/종이 조각.cc
@@ -12,28 +12,27 @@ int main(){
         }
     }
     
-    for(int s = 0; s < (1 << (n * m)); s++){
+    for(int b = 0; b < (1 << 16); b++){
         int sum = 0;
         for(int i = 0; i < n; i++){
             int cur = 0;
             for(int j = 0; j < m; j++){
                 int k = i * m + j;
-                if((s & (1 << k)) == 0){
-                    cur = cur * 10 + a[i][j];
-                }else{
+                if(b & (1 << k)){
                     sum += cur;
                     cur = 0;
+                }else{
+                    cur = cur * 10 + a[i][j];
                 }
             }
             sum += cur;
         }
-        
-        for(int j = 0; j < m; j++){
+        for(int i = 0; i < m; i++){
             int cur = 0;
-            for(int i = 0; i < n; i++){
-                int k = i * m + j;
-                if((s & (1 << k)) != 0){
-                    cur = cur * 10 + a[i][j];
+            for(int j = 0; j < n; j++){
+                int k = j * m + i;
+                if(b & (1 << k)){
+                    cur = cur * 10 + a[j][i];
                 }else{
                     sum += cur;
                     cur = 0;
