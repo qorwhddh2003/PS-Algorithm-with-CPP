8866db34582f890817175c77801b1d9ed8f06316
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-02-28 23:06:57 +0900

    [Silver V] Title: 집합, Time: 708 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -1,48 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-int mask;
+int n, temp, ret;
 string s;
 
-vector<string> split(const string& input, string delimiter){
-    vector<string> result;
-    auto start = 0;
-    auto end = input.find(delimiter);
-    while(end != string::npos){
-        result.push_back(input.substr(start, end - start));
-        start = end + delimiter.size();
-        end = input.find(delimiter, start);
-    }
-    result.push_back(input.substr(start));
-    return result;
-}
-
-int main()
-{
+int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     cout.tie(NULL);
     cin >> n;
-    for(int i = 0; i <= n; i++){
-        getline(cin, s);
-        vector<string> v = split(s, " ");
-        if(v[0] == "add"){
-            mask |= (1 << (stoi(v[1]) - 1));
-        }else if(v[0] == "remove"){
-            mask &= ~(1 << (stoi(v[1]) - 1));
-        }else if(v[0] == "check"){
-            if(mask & (1 << stoi(v[1]) - 1)) cout << 1 << "\n";
+    for(int i = 0; i < n; i++){
+        cin >> s;
+        // cout << i << " : " << s << " ";
+        if(s == "add"){
+            cin >> temp;
+            ret |= (1 << (temp - 1));
+        }else if(s == "remove"){
+            cin >> temp;
+            ret &= ~(1 << (temp - 1));
+        }else if(s == "check"){
+            cin >> temp;
+            if(ret & (1 << (temp - 1))) cout << 1 << "\n";
             else cout << 0 << "\n";
-        }else if(v[0] == "toggle"){
-            if(mask & (1 << stoi(v[1]) - 1)) mask &= ~(1 << (stoi(v[1]) - 1));
-            else mask |= (1 << (stoi(v[1]) - 1));
-        }else if(v[0] == "all"){
-            for(int j = 0; j < 20 ; j++){
-                mask |= (1 << j);
-            }
-        }else if(v[0] == "empty"){
-            mask = 0;
+        }else if(s == "toggle"){
+            cin >> temp;
+            ret ^= (1 << (temp - 1));
+        }else if(s == "all"){
+            ret = (1 << 20) - 1;
+        }else if(s == "empty"){
+            ret = 0;
         }
     }
     
