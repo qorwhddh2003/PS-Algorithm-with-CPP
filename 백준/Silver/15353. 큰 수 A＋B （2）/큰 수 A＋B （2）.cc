fe4bf4dcf791910d9d316e4a28ba5475620ebce6
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-03-02 12:49:13 +0900

    [Silver III] Title: 큰 수 A+B (2), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15353. 큰 수 A＋B （2）/큰 수 A＋B （2）.cc
+++ 백준/Silver/15353. 큰 수 A＋B （2）/큰 수 A＋B （2）.cc
@@ -1,32 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string a, b;
+string a, b, ret;
 
-string string_add(string a, string b){
-    int sum = 0;
+string add_string(string a, string b){
     string ret;
+    int sum = 0;
     while(a.size() || b.size() || sum){
         if(a.size()){
-            sum += a.back() - '0';
+            sum += (a.back() - '0');
             a.pop_back();
         }
         if(b.size()){
-            sum += b.back() - '0';
+            sum += (b.back() - '0');
             b.pop_back();
         }
-        ret += (sum % 10) + '0';
+        
+        ret += ((sum % 10) + '0');
         sum /= 10;
     }
-    reverse(ret.begin(), ret.end());
+    
     return ret;
-} 
+}
 
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    cout.tie(NULL);
     cin >> a >> b;
-    cout << string_add(a, b);
+    ret = add_string(a, b);
+    reverse(ret.begin(), ret.end());
+    cout << ret;
     return 0;
 }
