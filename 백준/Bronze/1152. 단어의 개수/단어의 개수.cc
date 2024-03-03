fc450d9bab8766604cfda563db6d974c093ab104
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-03-03 23:02:17 +0900

    [Bronze II] Title: 단어의 개수, Time: 8 ms, Memory: 3680 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int ret;
+string s;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    while(cin >> s){
+       ret++;
+    }
+    cout << ret;
+    return 0;
+}
