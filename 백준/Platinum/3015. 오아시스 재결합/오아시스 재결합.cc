35dc291f7be21f060725973043711abaf2e39c58
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-03-03 12:29:26 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 72 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll n, temp, ret;
+stack<pair<ll, ll>> stk;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> temp;
+        int cnt = 1;
+        while(stk.size() && stk.top().first <= temp){
+            ret += stk.top().second;
+            if(stk.top().first == temp){
+                cnt = stk.top().second + 1;
+            }else{
+                cnt = 1;
+            }
+            stk.pop();
+        }
+        if(stk.size()) ret++;
+        stk.push({temp, cnt});
+    }
+    cout << ret;
+    return 0;
+}
