1f3f5d1b7f5d757b5040c8877e835d3eef2b1d6e
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-03-05 18:49:25 +0900

    [Gold III] Title: 경사로, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.cc
+++ 백준/Gold/14890. 경사로/경사로.cc
@@ -2,35 +2,36 @@
 using namespace std;
 
 const int MAX = 101;
-int n, l, temp, ret;
-int board[MAX][MAX], rotated_board[MAX][MAX];
+int n, l, ret;
+int road[MAX][MAX], rotate_road[MAX][MAX];
 
-void go(int a[][MAX]){
+void go(int a[MAX][MAX]){
     for(int i = 0; i < n; i++){
-        int cnt = 1, j = 0;
+        int j = 0, cnt = 1;
         for(j = 0; j < n - 1; j++){
             if(a[i][j] == a[i][j + 1]) cnt++;
             else if(a[i][j] + 1 == a[i][j + 1] && cnt >= l) cnt = 1;
             else if(a[i][j] == a[i][j + 1] + 1 && cnt >= 0) cnt = -l + 1;
             else break;
         }
-        if(cnt >= 0 && j == n - 1) ret++;
+        if(j == n - 1 && cnt >= 0) ret++;
     }
 }
 
-int main(){
+int main()
+{
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     cout.tie(NULL);
     cin >> n >> l;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            cin >> temp;
-            board[i][j] = rotated_board[j][i] = temp;
+            cin >> road[i][j];
+            rotate_road[j][i] = road[i][j];
         }
     }
-    go(board);
-    go(rotated_board);
+    go(road);
+    go(rotate_road);
     cout << ret;
     return 0;
 }
