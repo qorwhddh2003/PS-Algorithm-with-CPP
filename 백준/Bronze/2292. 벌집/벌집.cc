645d367a204460a465ac6594493d028fca302438
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-03-07 18:46:58 +0900

    [Bronze II] Title: 벌집, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.cc
+++ 백준/Bronze/2292. 벌집/벌집.cc
@@ -1,19 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, cnt, st = 1;
+int n, cnt = 1, room_no = 1;
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     cout.tie(NULL);
     cin >> n;
-    if(n == 1){cout << 1; return 0;}
     while(true){
-        if(st >= n && n <= st + cnt * 6) break;
-        st += cnt * 6;
+        if(n <= room_no) break;
+        room_no += (6 * cnt);
         cnt++;
     }
-    cout << cnt;    
+    cout << cnt;
     return 0;
 }
