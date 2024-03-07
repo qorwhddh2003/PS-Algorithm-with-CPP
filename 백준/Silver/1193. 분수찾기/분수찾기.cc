6ba07d3b89cb0d76b4290e9c14aed6e907890262
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-03-07 19:03:10 +0900

    [Silver V] Title: 분수찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.cc
+++ 백준/Silver/1193. 분수찾기/분수찾기.cc
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, k = 1; 
+int n, k = 1;
 int main()
 {
     ios_base::sync_with_stdio(0);
@@ -15,9 +15,9 @@ int main()
     
     int a = k * (k + 1) / 2 - n;
     if(k % 2){
-        cout << 1 + a << "/" <<  k - a;
+        cout << a + 1 << "/" << k - a;
     }else{
-        cout << k - a << "/" << 1 + a;
+        cout << k - a << "/" << a + 1;
     }
     return 0;
 }
