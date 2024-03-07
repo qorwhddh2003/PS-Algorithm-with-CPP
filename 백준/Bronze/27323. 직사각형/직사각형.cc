a68be31afdde3ccbc9d79972d261d64dee08be9a
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-03-07 22:23:44 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> a >> b;
+    cout << a * b;
+    return 0;
+}
