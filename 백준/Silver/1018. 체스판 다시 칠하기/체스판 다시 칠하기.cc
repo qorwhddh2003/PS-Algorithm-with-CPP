0ef9d3484c47562719df87f7027435b888ce88fa
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-03-09 23:03:05 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -4,15 +4,15 @@ using namespace std;
 int n, m, ret = 9e8;
 char a[51][51];
 
-int checkBoard(int y, int x, char bw){
+int check(int y, int x, char color){
     int cnt = 0;
     for(int i = y; i < y + 8; i++){
         for(int j = x; j < x + 8; j++){
-            if((i + j) % 2 == 0 && a[i][j] == bw) cnt++;
-            else if((i + j) % 2 != 0 && a[i][j] != bw) cnt++;
+            if((i + j) % 2 == 0 && a[i][j] != color) cnt++;
+            if((i + j) % 2 == 1 && a[i][j] == color) cnt++;
         }
     }
-    return 64 - cnt;
+    return cnt;
 }
 
 int main()
@@ -29,10 +29,11 @@ int main()
     
     for(int i = 0; i <= n - 8; i++){
         for(int j = 0; j <= m - 8; j++){
-            ret = min(ret, checkBoard(i, j, 'W'));
-            ret = min(ret, checkBoard(i, j, 'B'));
+            ret = min(ret, check(i, j, 'W'));
+            ret = min(ret, check(i, j, 'B'));
         }
     }
+    
     cout << ret;
     return 0;
 }
