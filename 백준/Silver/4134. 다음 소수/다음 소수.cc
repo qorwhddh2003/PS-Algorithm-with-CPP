df61f7798e7175eec5ccdff0e01068ab4c15fe03
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-03-12 22:38:26 +0900

    [Silver IV] Title: 다음 소수, Time: 248 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.cc
+++ 백준/Silver/4134. 다음 소수/다음 소수.cc
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-const ll INF = 4 * 1e9;
 ll t, n;
 
 int main()
@@ -12,7 +11,6 @@ int main()
     cin >> t;
     while(t--){
         cin >> n;
-        
         if(n <= 2){
             cout << 2 << "\n";
             continue;
@@ -20,13 +18,12 @@ int main()
         
         for(ll i = n;; i++){
             bool flag = true;
-            for(int j = 2; j < sqrt(i) + 1; j++){
+            for(ll j = 2; j * j <= i; j++){
                 if(i % j == 0){
                     flag = false;
                     break;
                 }
             }
-            
             if(flag){
                 cout << i << "\n";
                 break;
