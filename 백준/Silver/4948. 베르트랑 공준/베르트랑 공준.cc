e69e6bce6a45508a6e3f95d8547907b64801c16f
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-03-13 22:26:45 +0900

    [Silver II] Title: 베르트랑 공준, Time: 4 ms, Memory: 2260 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
@@ -3,20 +3,19 @@ using namespace std;
 
 const int MAX = 123456 * 2 + 1;
 int n, m;
-bool isPrime[MAX];
-
+bool ok[MAX];
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     cout.tie(NULL);
     
-    fill(isPrime, isPrime + MAX, 1);
-    isPrime[1] = false;
-    for(int i = 2; i * i <= MAX; i++){
-        if(isPrime[i]){
-            for(int j = i * i; j <= MAX; j += i){
-                isPrime[j] = false;
+    fill(ok, ok + MAX, 1);
+    ok[1] = false;
+    for(int i = 2; i * i < MAX; i++){
+        if(ok[i]){
+            for(int j = i * i; j < MAX; j += i){
+                ok[j] = false;
             }
         }
     }
@@ -24,13 +23,12 @@ int main()
     while(cin >> n){
         if(n == 0) break;
         int ret = 0;
-        for(int i = n + 1; i <= n * 2; i++){
-            if(isPrime[i]){
+        for(int i = n + 1; i <= 2 * n; i++){
+            if(ok[i]){
                 ret++;
             }
         }
         cout << ret << "\n";
     }
-    
     return 0;
 }
