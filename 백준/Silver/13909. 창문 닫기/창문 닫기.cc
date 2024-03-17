b6731faef4406ff093d7f6a8a7af096b0d7cc148
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-03-17 23:16:35 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/13909. 창문 닫기/창문 닫기.cc
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -1,18 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, ret;
+int N, ret;
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     cout.tie(NULL);
-    
-    cin >> n;
-    for(int i = 1; i <= sqrt(n); i++){
+    cin >> N;
+    for(int i = 1; i * i <= N; i++){
         ret++;
     }
-    
     cout << ret;
     return 0;
 }
