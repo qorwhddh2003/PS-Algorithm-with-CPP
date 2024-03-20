32af6a9ffbcd4a0c7a8b1e1972285186d8cf7b92
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-03-20 23:01:21 +0900

    [Silver IV] Title: 다음 소수, Time: 244 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.cc
+++ 백준/Silver/4134. 다음 소수/다음 소수.cc
@@ -1,21 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 typedef long long ll;
-ll  t, n;
+ll T, n;
+
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     cout.tie(NULL);
-    cin >> t;
-    while(t--){
+    cin >> T;
+    while(T--){
         cin >> n;
-        
         if(n <= 2){
             cout << 2 << "\n";
             continue;
         }
-        
         for(ll i = n;; i++){
             bool flag = true;
             for(ll j = 2; j * j <= i; j++){
@@ -30,6 +30,6 @@ int main()
             }
         }
     }
-
+    
     return 0;
 }
