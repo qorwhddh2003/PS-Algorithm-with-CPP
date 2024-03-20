2909f502aac6e0b3ad5c2eb61a2886603e6b81c0
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-03-20 23:05:33 +0900

    [Silver III] Title: 소수 구하기, Time: 12 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -2,29 +2,27 @@
 using namespace std;
 
 const int MAX = 1000001;
-int n, m;
-bool isPrime[MAX];
+int m, n;
+bool is_prime[MAX];
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     cout.tie(NULL);
-    cin >> n >> m;
-    fill(isPrime, isPrime + MAX, 1);
-    isPrime[1] = false;
-    for(int i = 2; i * i <= m; i++){
-        if(isPrime[i]){
-            for(int j = i * i; j <= m; j += i){
-                isPrime[j] = false;
-            }
+    cin >> m >> n;
+    
+    is_prime[1] = true;
+    for(int i = 2; i * i < MAX; i++){
+        if(is_prime[i]) continue;
+        for(int j = i * i; j < MAX; j += i){
+            is_prime[j] = true;
         }
     }
     
-    for(int i = n; i <= m; i++){
-        if(isPrime[i]){
-            cout << i << "\n";
-        }
+    for(int i = m; i <= n; i++){
+        if(is_prime[i]) continue;
+        cout << i << "\n";
     }
     
     return 0;
