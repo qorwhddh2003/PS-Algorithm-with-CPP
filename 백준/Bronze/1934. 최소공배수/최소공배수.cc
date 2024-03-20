79b6e91f3cb98a47cb67e93338dadb73b311b111
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-03-20 22:34:08 +0900

    [Bronze I] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.cc
+++ 백준/Bronze/1934. 최소공배수/최소공배수.cc
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int t, a, b;
+int T, a, b;
 
 int gcd(int a, int b){
     if(a == 0) return b;
-    return gcd(b % a, a);
+    return gcd(b % a , a);
 }
 
 int lcm(int a, int b){
@@ -17,11 +17,10 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     cout.tie(NULL);
-    cin >> t;
-    while(t--){
+    cin >> T;
+    while(T--){
         cin >> a >> b;
         cout << lcm(a, b) << "\n";
     }
-    
     return 0;
 }
