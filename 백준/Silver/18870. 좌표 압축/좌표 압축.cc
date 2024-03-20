706d4218b5f36a4d472de799de7db3f0f53025af
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-03-20 22:32:23 +0900

    [Silver II] Title: 좌표 압축, Time: 472 ms, Memory: 12524 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,10 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define prev aaaa
 
 int n, temp;
-vector<int> v1;
-vector<int> v2;
+vector<int> v1, v2;
+
 int main()
 {
     ios_base::sync_with_stdio(0);
@@ -16,10 +15,10 @@ int main()
         v1.push_back(temp);
         v2.push_back(temp);
     }
-    sort(v1.begin(), v1.end());
-    v1.erase(unique(v1.begin(), v1.end()), v1.end());
+    sort(v2.begin(), v2.end());
+    v2.erase(unique(v2.begin(), v2.end()), v2.end());
     for(int i = 0; i < n; i++){
-        cout << lower_bound(v1.begin(), v1.end(), v2[i]) - v1.begin() << " ";
+        cout << lower_bound(v2.begin(), v2.end(), v1[i]) - v2.begin() << " ";
     }
     return 0;
 }
