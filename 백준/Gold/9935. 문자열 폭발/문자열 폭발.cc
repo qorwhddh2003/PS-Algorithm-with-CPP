988295b7202dc908df13a80d7db78662abb97b0b
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-03-24 18:57:10 +0900

    [Gold IV] Title: 문자열 폭발, Time: 20 ms, Memory: 7004 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -1,38 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-string str, remove_str, ret;
+string a, b;
 stack<char> stk;
+
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     cout.tie(NULL);
-    cin >> str >> remove_str;
-    for(int i = 0; i < str.size(); i++){
-        stk.push(str[i]);
-        if(stk.size() >= remove_str.size() && stk.top() == remove_str[remove_str.size() - 1]){
-            string temp = "";
-            for(int j = 0; j < remove_str.size(); j++){
-                temp += stk.top();
-                stk.pop();
+    cin >> a >> b;
+    for(char ch : a){
+        stk.push(ch);
+        if(stk.size() >= b.size() && stk.top() == b[b.size() - 1]){
+            string tempS = "";
+            for(char c : b){
+                tempS += stk.top(); stk.pop();
             }
-            reverse(temp.begin(), temp.end());
-            
-            if(temp != remove_str){
-                for(char ch : temp){
-                    stk.push(ch);
+            reverse(tempS.begin(), tempS.end());
+            if(tempS != b){
+                for(char c : tempS){
+                    stk.push(c);
                 }
             }
         }
     }
     
-    while(stk.size()){
-        ret += stk.top(); 
-        stk.pop();
-    }
-    reverse(ret.begin(), ret.end());
-    cout << (ret.size() ? ret : "FRULA");
+    if(stk.size()){
+        string ret = "";
+        while(stk.size()){
+            ret += stk.top(); stk.pop();
+        }
+        reverse(ret.begin(), ret.end());
+        cout << ret;
+    }else cout << "FRULA";
     return 0;
 }
