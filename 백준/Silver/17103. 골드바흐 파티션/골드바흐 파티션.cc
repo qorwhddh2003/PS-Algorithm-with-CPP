6e6342d4a2dfc5be1734e823e87d150a72a78d96
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-03-24 18:30:42 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 48 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.cc
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.cc
@@ -1,36 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 1e6;
-int t, n, ret;
-bool isPrime[MAX + 1];
-vector<int> prime_numbers;
+const int MAX = 1000000;
+int T, n;
+bool is_prime[MAX + 1];
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     cout.tie(NULL);
     
-    fill(isPrime, isPrime + MAX + 1, 1);
-    isPrime[1] = false;
-    for(int i = 2; i * i <= MAX; i++){
-        if(!isPrime[i]) continue;
-        for(int j = i * i; j <= MAX; j += i){
-            isPrime[j] = false;
+    is_prime[1] = true;
+    for(int i = 2; i * i < MAX + 1; i++){
+        if(is_prime[i]) continue;
+        for(int j = i * i; j < MAX + 1; j += i){
+            is_prime[j] = true;
         }
     }
     
-    cin >> t;
-    while(t--){
+    cin >> T;
+    while(T--){
         cin >> n;
-        ret = 0;
-        for(int i = 0; i <= n / 2; i++){
-            if(isPrime[i] && isPrime[n - i]){
+        int ret = 0;
+        for(int i = 1; i <= n / 2; i++){
+            if(!is_prime[i] && !is_prime[n - i]){
                 ret++;
             }
         }
         cout << ret << "\n";
     }
-    
     return 0;
 }
