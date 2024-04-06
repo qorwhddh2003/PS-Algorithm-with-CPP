f1be30a5043b3b46bd9a02a129f4238bdc67a6ff
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-04-06 23:36:05 +0900

    [Silver I] Title: 부등호, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.cc
+++ 백준/Silver/2529. 부등호/부등호.cc
@@ -1,62 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 typedef long long ll;
-
-ll n, mx = 0, mn = 1e18;
-int visited[10];
-char c;
-vector<ll> num;
+ll K, mx_ret, mn_ret = 9e10;
 vector<char> oper_str;
+char temp;
 
-void print(ll _mx, ll _mn){
-    string mx_str = "0" + to_string(mx);
-    string mn_str = "0" + to_string(mn);
-    mx_str = mx_str.substr(mx_str.size() - (n + 1));
-    mn_str = mn_str.substr(mn_str.size() - (n + 1));
-    cout << mx_str << "\n" << mn_str;
+string print(ll num){
+    string temp = "00" + to_string(num);
+    return temp.substr(temp.size() - K - 1);
 }
 
 bool oper(char a, ll b, ll c){
-    if(a == '<'){
-        if(b < c) return true;
-        else return false;
-    }else if(a == '>'){
-        if(b > c) return true;
-        else return false;
-    }
+    if(a == '<') return b < c;
+    else return b > c;
 }
 
-void go(ll here, ll _num, ll ret){
-    ret += (_num * pow(10, n - here));
-    if(here == n){
-        mx = max(mx, ret);
-        mn = min(mn, ret);
+void go(ll here, ll ret, ll visited){
+    if(here == K){
+        mx_ret = max(mx_ret, ret);
+        mn_ret = min(mn_ret, ret);
         return;
     }
-    for(int i = 0; i <= 9; i++){
-        if(visited[i]) continue;
-        if(oper(oper_str[here], _num, num[i])){
-            visited[i] = 1;
-            go(here + 1, num[i], ret);
-            visited[i] = 0;
+    
+    for(ll i = 0; i <= 9; i++){
+        if(visited & (1 << i)) continue;
+        if(here == -1 || oper(oper_str[here], to_string(ret).back() - '0', i)){
+            go(here + 1, ret * 10 + i, visited | (1 << i));
         }
     }
-    return;
 }
 
 int main(){
     ios_base::sync_with_stdio(0);
-    cin >> n;
-    for(int i = 0; i < n; i++){
-        cin >> c;
-        oper_str.push_back(c);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> K;
+    for(ll i = 0; i < K; i++){
+        cin >> temp;
+        oper_str.push_back(temp);
     }
-    for(int i = 0; i <= 9; i++) num.push_back(i);
-    for(int i = 0; i <= 9; i++){
-        visited[i] = 1;
-        go(0, i, 0);
-        visited[i] = 0;
-    }
-    print(mx, mn);
+    
+    go(-1, {}, 0);
+    cout << print(mx_ret) << "\n" << print(mn_ret);
     return 0;
 }
