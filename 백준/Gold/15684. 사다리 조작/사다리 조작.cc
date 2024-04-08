ad07cb5b0d2f5848f48de28ec91a9a6f7fcaa2db
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-04-08 14:17:53 +0900

    [Gold III] Title: 사다리 조작, Time: 992 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.cc
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.cc
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int INF = 987654321;
-int n, m, h, a, b, ret = INF;
-int visited[31][31];
+const int INF = 9e8;
+int N, M, H, a, b, ret = INF;
+int visited[34][14];
 
 bool check(){
-    for(int i = 1; i <= n; i++){
+    for(int i = 1; i <= N; i++){
         int start = i;
-        for(int j = 1; j <= h; j++){
+        for(int j = 1; j <= H; j++){
             if(visited[j][start]) start++;
             else if(visited[j][start - 1]) start--;
         }
@@ -17,17 +17,17 @@ bool check(){
     return true;
 }
 
-void go(int here, int cnt){
-    if(cnt > 3 || cnt >= ret) return;
+void ladder(int here, int count){
+    if(count > 3 || count >= ret) return;
     if(check()){
-        ret = min(ret, cnt);
+        ret = min(ret, count);  
         return;
-    }
-    for(int i = here; i <= h; i++){
-        for(int j = 1; j < n; j++){
+    } 
+    for(int i = here; i <= H; i++){
+        for(int j = 1; j < N; j++){
             if(visited[i][j] || visited[i][j - 1] || visited[i][j + 1]) continue;
             visited[i][j] = 1;
-            go(i, cnt + 1);
+            ladder(i, count + 1);
             visited[i][j] = 0;
         }
     }
@@ -37,12 +37,14 @@ int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     cout.tie(NULL);
-    cin >> n >> m >> h;
-    for(int i = 0; i < m; i++){
+    
+    cin >> N >> M >> H;
+    for(int i = 0; i < M; i++){
         cin >> a >> b;
         visited[a][b] = 1;
     }
-    go(1, 0);
+    
+    ladder(1, 0);
     cout << (ret == INF ? -1 : ret);
     return 0;
 }
