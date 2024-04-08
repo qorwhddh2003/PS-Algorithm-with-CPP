1eb7dda28e8d7f377b3149a9ce3ff3c83138f27c
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-04-08 15:57:08 +0900

    [Gold IV] Title: 게리맨더링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.cc
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.cc
@@ -1,20 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 14, INF = 9e8;
-int n, m, a, temp, ret = INF;
-int population[MAX], visited[MAX], group[MAX];
-vector<int> v[MAX];
+const int MAX = 10;
+int N, M, temp, red, blue, ret = 9e8;
+int population[MAX], team[MAX], visited[MAX];
+vector<int> adj[MAX];
 
-pair<int, int> dfs(int here, int group_num){
+pair<int, int> dfs(int here, int color){
     visited[here] = 1;
     pair<int, int> ret = {1, population[here]};
-    for(int there : v[here]){
-        if(visited[there]) continue;
-        if(group[there] != group_num) continue;
-        pair<int, int> _temp = dfs(there, group_num);
-        ret.first += _temp.first;
-        ret.second += _temp.second;
+    for(int there : adj[here]){
+        if(visited[there] || color != team[there]) continue;
+        pair<int, int> temp = dfs(there, color);
+        ret.first += temp.first;
+        ret.second += temp.second;
     }
     return ret;
 }
@@ -23,37 +22,37 @@ int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     cout.tie(NULL);
-    cin >> n;
-    for(int i = 1; i <= n; i++){
+    cin >> N;
+    for(int i = 0; i < N; i++){
         cin >> population[i];
     }
     
-    for(int i = 1; i <= n; i++){
-        cin >> m;
-        for(int j = 0; j < m; j++){
+    for(int i = 0; i < N; i++){
+        cin >> M;
+        for(int j = 0; j < M; j++){
             cin >> temp;
-            v[i].push_back(temp);
-            v[temp].push_back(i);
+            adj[i].push_back(temp - 1);
+            adj[temp - 1].push_back(i);
         }
     }
     
-    for(int i = 1; i <= (1 << (n - 1)); i++){
-        memset(visited, 0, sizeof(visited));
-        memset(group, 0, sizeof(group));
-        int startA = 0, startB = 0;
-        for(int j = 0; j < n; j++){
+    for(int i = 1; i <= (1 << (N - 1)); i++){
+        fill(visited, visited + MAX, 0);
+        fill(team, team + MAX, 0);
+        for(int j = 0; j < N; j++){
             if(i & (1 << j)){
-                group[j + 1] = 1;
-                startA = j + 1;
-            }else startB = j + 1;
+                team[j] = 1;
+                red = j;
+            }else{
+                blue = j;
+            }
         }
-        pair<int, int> blue = dfs(startA, 1);
-        pair<int, int> red = dfs(startB, 0);
-        if(blue.first + red.first == n){
-            ret = min(ret, abs(blue.second - red.second));
+        pair<int, int> red_team = dfs(red, 1);
+        pair<int, int> blue_team = dfs(blue, 0);
+        if(red_team.first + blue_team.first == N){
+            ret = min(ret, abs(red_team.second - blue_team.second));
         }
     }
-    
-    cout << (ret == INF ? -1 : ret);
+    cout << (ret == 9e8 ? -1 : ret);
     return 0;
 }
