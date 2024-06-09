6470b9e7e797bd81927d224d8140a0c626f505a2
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2024-06-09 23:23:00 +0900

    [Silver II] Title: 좌표 압축, Time: 468 ms, Memory: 12524 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,24 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, temp;
-vector<int> v1, v2;
+int N, temp;
+vector<int> v, vv;
 
-int main()
-{
+void fastIO(){
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     cout.tie(NULL);
-    cin >> n;
-    for(int i = 0; i < n; i++){
+}
+
+
+int main()
+{
+    fastIO();
+    cin >> N;
+    for(int i = 0; i < N; i++){
         cin >> temp;
-        v1.push_back(temp);
-        v2.push_back(temp);
+        v.push_back(temp);
+        vv.push_back(temp);
     }
-    sort(v2.begin(), v2.end());
-    v2.erase(unique(v2.begin(), v2.end()), v2.end());
-    for(int i = 0; i < n; i++){
-        cout << lower_bound(v2.begin(), v2.end(), v1[i]) - v2.begin() << " ";
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+    for(int i : vv){
+        cout << lower_bound(v.begin(), v.end(), i) - v.begin() << " ";
     }
     return 0;
 }
