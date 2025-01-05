0075430ee58d359549e679f185cd2df4b453af5f
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2025-01-05 14:58:20 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -2,16 +2,12 @@
 using namespace std;
 
 string str, rev_str;
-int main(){
+
+int main()
+{
     cin >> str;
-    rev_str = str;
+    rev_str =str;
     reverse(rev_str.begin(), rev_str.end());
-    
-    if(str == rev_str){
-        cout << 1;
-    }else{
-        cout << 0;
-    }
-    
+    cout << (str == rev_str);
     return 0;
 }
